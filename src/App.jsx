32561d20699fd039fc017ca6aa05ffbc5d06a37b
Ademola <oladipupoademola09@mail.com>
2024-09-10 11:24:46 +0100

    first upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import HomePage from "./routes/homePage/homePage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage";
+
+
+
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: 
+       <Layout/>,
+       children: [
+        {
+          path:"/",
+          element:<HomePage/>
+        },
+        {
+          path:"/list",
+          element:<ListPage/>
+        },
+        {
+          path:"/:id",
+          element:<SinglePage/>
+        }
+       ]
+    },
+   
+  ]);
+
   return (
-    <div>Hello World</div>
-  )
+    
+    <RouterProvider router={router}/>
+  );
 }
 
 export default App
---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,37 @@
+import SearchBar from '../../components/searchBar/SearchBar'
+import './homePage.scss'
+
+function HomePage(){
+    return (
+        <div className='homePage'>
+            <div className="textContainer">
+                <div className="wrapper">
+                <h1 className='title'>Find Real Estate & Get Your Dream Place</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, adipisci natus. Consectetur quia id dolore quasi assumenda optio labore hic porro dolorum, quibusdam ab accusantium recusandae odio reprehenderit quo at!
+               .</p>
+               <SearchBar/>
+               <div className="boxes">
+                <div className="box">
+                    <h1>16+</h1>
+                    <h2>Years of Experience</h2>
+                </div>
+                <div className="box">
+                    <h1>200</h1>
+                    <h2>Award Gained</h2>
+                </div>
+                <div className="box">
+                    <h1>2000+</h1>
+                    <h2>Property Ready</h2>
+                </div>
+               </div>
+                </div>
+               
+            </div>
+            <div className="imgContainer">
+                <img src="/bg.png" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
